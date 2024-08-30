3d7afb07ade8c0fbe440dc312093ab47cce69798
Iee0331 <ths7789@naver.com>
2024-08-31 07:45:28 +0900

    샌즈 만들었어요.
    ㅇㅇ

---
+++ Assets/Scripts/New Folder/BulletCopy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCopy : MonoBehaviour
+{
+    private float _Dmg;
+    public float Dmg { get { return _Dmg; } set {  _Dmg = value; } }
+
+    private string _MyObj;
+    public string MyObj {  get { return _MyObj; } set {  _MyObj = value; } }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_MyObj == collision.name) return;
+
+        if(collision.gameObject.GetComponent<Monster>())
+            collision.gameObject.GetComponent<Monster>().takeDamage(_Dmg);
+
+        if(collision.tag!="Bullet")
+            Destroy(gameObject);
+    }
+}
